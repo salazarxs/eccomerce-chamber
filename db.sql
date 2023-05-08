a95a47e5fe06fd0e4d7af0eb00013ea095c10ad8
chiral <martinpaolo10@gmail.com>
2023-05-08 18:36:55 -0400

    galeria

---
+++ db.sql
@@ -0,0 +1,8 @@
+create table productos(
+    id int primary key auto_increment,
+    name varchar(100),
+    price varchar(100),
+    rating varchar(100),
+    description varchar(200),
+    img varchar(100)
+);
